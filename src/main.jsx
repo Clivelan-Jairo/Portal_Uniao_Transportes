b6dab8763c34b807ecaa2951eb4450e59114848d
Clivelan Jairo <jairoclivelan@gmail.com>
2025-11-16 21:31:22 -0300

    atualizaçoes

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,14 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 
+// Definir variável CSS --vh baseada na altura da viewport
+function setVhVariable() {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+setVhVariable();
+window.addEventListener('resize', setVhVariable);
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
