08f675d4b402f7a9705ba675277cb33aa5f96244
Jairo <jairoclivelan@gmail.com>
2026-01-07 10:41:09 -0300

    correçoes

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer>
-      <p>© 2025 União Transportes. Todos os direitos reservados.</p>
+      <p>© 2026 União Transportes. Todos os direitos reservados.</p>
       <p><a href="#">Política de Privacidade</a> | <a href="#">Termos de Uso</a></p>
     </footer>
   );
